a4018d7a97b5a9d2be9953221c6b96a79c33d228
zrotof <manduel21@gmail.com>
2023-10-06 04:58:13 +0200

    include dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://elec-api.foryoumedia-africa.xyz/api/",
+  liveStreaming : "https://stream.az-multimedia.com:3793/live/foryoutvlive.m3u8"
 };
