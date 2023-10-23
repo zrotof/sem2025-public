35d06c5fcfb3bdd5cacbdfe9ac985da955aafdc6
zrotof <manduel21@gmail.com>
2023-10-23 19:50:35 +0200

    adding live link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  liveStreaming : "https://stream.az-multimedia.com:3793/live/foryoutvlive.m3u8",
+  liveStreaming : "https://stream.az-multimedia.com:3305/live/semtvlive.m3u8",
   apiBaseUrl : "https://www.api-test.amadou-ba.sn/v1/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  liveStreaming : "https://stream.az-multimedia.com:3793/live/foryoutvlive.m3u8",
+  liveStreaming : "https://stream.az-multimedia.com:3305/live/semtvlive.m3u8",
   apiBaseUrl : "http://localhost:3000/v1/"
 };
 
